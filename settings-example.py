2313f57a249cb6db2fcee527df49163cc3d33883
Jinxuan Cui <i@cjxol.com>
2021-01-05 21:13:20 +0800

    Update settings example one-line comments

--- settings-example.py
+++ settings-example.py
@@ -1,22 +1,16 @@
 import os
 
 
-'''
-Bot Configration
-'''
+'''Bot Configration'''
 DB_PATH = os.path.dirname(os.path.abspath(__file__)) + 'db.sqlite3'
 COMMAND_PREFIX = '.'
 MINECRAFT_SERVER_OFFLINE_ALERT_CHANNEL_IDS = []
 
 
-'''
-Minecraft server infomation
-'''
+'''Minecraft server infomation'''
 MINECRAFT_SERVER_ADDRESS = 'localhost'
 MINECRAFT_SERVER_PORT = 25565
 
 
-'''
-Discord TOKEN
-'''
+'''Discord TOKEN'''
 DISCORD_BOT_TOKEN = ''
