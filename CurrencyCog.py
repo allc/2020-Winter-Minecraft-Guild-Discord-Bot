0a85c33f9e158674d3ea64fcd675062d4bdf9e45
Jinxuan Cui <i@cjxol.com>
2021-01-14 12:36:18 +0800

    Add balance and timely command for Currency Cog

--- CurrencyCog.py
+++ CurrencyCog.py
@@ -1,10 +1,12 @@
+import discord
 from discord.ext import commands
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from models import CurrencyPlayer
+from datetime import datetime, timedelta
 
 
-class CurrencyCog(commands.Cog, name='Minecraft'):
+class CurrencyCog(commands.Cog, name='Currency'):
     def __init__(self, bot, db_engine):
         self.bot = bot
         self.db_engine = db_engine
@@ -22,4 +24,38 @@ class CurrencyCog(commands.Cog, name='Minecraft'):
         balance_amount_str = f'{balance_amount:,}'.replace(',', ' ')
         username = ctx.author.name
         discriminator = ctx.author.discriminator
-        await ctx.send(f'**{username}#{discriminator}** has has {balance_amount_str} :moneybag:')
+        embed = discord.Embed(description=f'**{username}#{discriminator}** has {balance_amount_str} :moneybag:', colour=discord.Colour.green())
+        await ctx.send(embed=embed)
+
+
+    @commands.command(brief='Claim some reward every 12h')
+    async def timely(self, ctx):
+        '''Claim reward if at least the required amount of time has passed since last time the reward has been claimed.
+        Add currency player record if no record found
+
+        '''
+        # Get currency player
+        additional_message = False
+        try:
+            Session = sessionmaker(bind=self.db_engine)
+            session = Session()
+            currency_player = session.query(CurrencyPlayer).filter(CurrencyPlayer.discord_user_id==ctx.author.id).one()
+        except NoResultFound:
+            currency_player = CurrencyPlayer(discord_user_id=ctx.author.id, balance=0, last_timely=datetime.fromtimestamp(0))
+            additional_message = '\nAre timelys even real :thonk:'
+        username = ctx.author.name
+        discriminator = ctx.author.discriminator
+        # Check if the required amount of time passed
+        current_time = datetime.utcnow()
+        if current_time - currency_player.last_timely < timedelta(hours=12):
+            time_left = timedelta(hours=12) - (current_time - currency_player.last_timely)
+            embed = discord.Embed(description=f'**{username}#{discriminator}** You\'ve already claimed your timely reward. You can get it again in {time_left.days}d {time_left.seconds // 3600}h {time_left.seconds // 60 % 60}m {time_left.seconds % 60}s.', colour=discord.Colour.red())
+        else:
+            currency_player.balance += 200
+            currency_player.last_timely = current_time
+            session.add(currency_player)
+            session.commit()
+            embed = discord.Embed(description=f'**{username}#{discriminator}** You\'ve claimed your 200:moneybag:. You can claim again in 12h', colour=discord.Colour.green())
+            if additional_message:
+                embed.set_footer(text=additional_message)
+        await ctx.send(embed=embed)
